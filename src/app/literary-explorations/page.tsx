4788966954d16766144f388feb2e85f9f745f1aa
Arpan Sethi <arpansethi30@gmail.com>
2024-08-13 23:19:35 +0530

    page

---
+++ src/app/literary-explorations/page.tsx
@@ -0,0 +1,8 @@
+// src/app/artistic-ventures/page.tsx
+import React from 'react';
+
+const Page = () => {
+  return <div>Literary</div>;
+};
+
+export default Page;
---
+++ src/app/technical-odyssey/page.tsx
@@ -0,0 +1,8 @@
+// src/app/artistic-ventures/page.tsx
+import React from 'react';
+
+const Page = () => {
+  return <div>Artistic Ventures Page</div>;
+};
+
+export default Page;
